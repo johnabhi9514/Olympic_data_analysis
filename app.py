9a32c4d6e7e31e9cfaa0b4d1f18f2a81f9ef85df
Abhishek Singh <62017340+johnabhi9514@users.noreply.github.com>
2022-03-24 15:12:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as data
-from tensorflow.keras.models import load_model
+import keras
+from keras.models import load_model
 import streamlit as st
 
 start = '2010-01-01'
